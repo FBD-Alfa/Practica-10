5fd3979d4d23bdd0cc4be2f0a1e923c984f5a88d
Jesús Alberto Reyes Gutiérrez <alberto.r@ciencias.unam.mx>
2022-05-25 22:36:58 -0500

    Prueba 2

--- SP.sql
+++ SP.sql
@@ -1,15 +1,15 @@
 --CREATE LANGUAGE plpgsql;
 SELECT * FROM veterinario;
 
-CREATE OR REPLACE PROCEDURE registrarVeterinario(curp IN CHAR(18),idEstetica IN INT, 
-									apellidoM IN VARCHAR(50), apellidoP IN VARCHAR(50),
-									nombre IN VARCHAR(50), estado IN VARCHAR(50),
-									calle IN VARCHAR(50), numero IN INT, cp IN INT,
-									telefono IN CHAR(10), genero IN CHAR(1), 
+CREATE OR REPLACE PROCEDURE registrarVeterinario(curp IN VARCHAR,idEstetica IN INT, 
+									apellidoM IN VARCHAR, apellidoP IN VARCHAR,
+									nombre IN VARCHAR, estado IN VARCHAR,
+									calle IN VARCHAR, numero IN INT, cp IN INT,
+									telefono IN CHAR, genero IN CHAR, 
 									nacimiento IN DATE, salario IN INT, 
-									rfc in VARCHAR(30), inicio IN VARCHAR(30),
-									fin IN VARCHAR(30), noPacientes IN INT,
-									fechaInicio IN DATE, fechaTermino IN INT)
+									rfc in VARCHAR, inicio IN VARCHAR,
+									fin IN VARCHAR, noPacientes IN INT,
+									fechaInicio IN DATE, fechaTermino IN DATE)
 	AS $$
 	BEGIN
 		IF (TRUE) THEN
@@ -20,8 +20,10 @@ CREATE OR REPLACE PROCEDURE registrarVeterinario(curp IN CHAR(18),idEstetica IN 
 	$$
 	Language plpgsql;
 
-CALL registrarVeterinario('ZHXY752436AAIJJQ83',26,'Barnhart','Chadd','Faye','Zacatecas','Rockefeller',23,16060,3045636873,'F','1985-07-09',12264,'MEPR796726K32','9:58 AM','12:16 AM',2,'2009-08-22','2017-03-28');
+CALL registrarVeterinario('ZHXY752436AAIJJQ83',26,'Barnhart','Chadd','Faye','Zacatecas','Rockefeller',23,16060,3045636873,'F','1985-07-09'::date,12264,'MEPR796726K32','9:58 AM','12:16 AM',2,'2009-08-22'::date,'2017-03-28'::date);
 
+
+SELECT * FROM cuentas WHERE curp = 'ZHXY752436AAIJJQ83';
 
 SELECT * FROM veterinario WHERE curp = 'ZHXY752436AAIJJQ83';
 
