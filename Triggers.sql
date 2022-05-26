4217f03daaab396712855ea7a7469bd178c61f8e
victor115 <victorvillalobos@ciencias.unam.mx>
2022-05-25 22:51:57 -0500

    Triggers terminados

--- Triggers.sql
+++ Triggers.sql
@@ -25,3 +25,37 @@ EXECUTE PROCEDURE swap_surnames();
 --Pruebas
 INSERT INTO supervisor(curp,idEstetica,apellidoM,apellidoP,nombre,estado,calle,numero,cp,telefono,genero,nacimiento,salario,rfc,inicio,fin,fechaInicio,fechaTermino) VALUES ('CACC636991RJPSEP27',3,'Pasek','Bettesworth','Gustav','Campeche','Mcguire',6,62707,4507792203,'M','1981-08-13',154470,'UZBN404892A7B','4:19 PM','10:05 PM','2015-05-04','2006-01-11');
 SELECT apellidoM, apellidoP FROM supervisor where curp='CACC636991RJPSEP27';
+
+--Funcion que manda una excepcion si se intenta eliminar o actualizar algun elemento de la tabla mascota 
+CREATE OR REPLACE FUNCTION prevent_del_upt() RETURNS TRIGGER
+AS
+$$
+BEGIN
+ IF TG_OP = 'DELETE' THEN
+ RAISE EXCEPTION 'La tabla mascota no permite eliminaciones';
+ END IF;
+ IF TG_OP = 'UPDATE' THEN
+ RAISE EXCEPTION 'La tabla mascota no permite actualizaciones';
+ END IF;
+ RETURN null;
+ END
+ $$
+LANGUAGE plpgsql;
+--Trigger que manda a llamar la funcion de procedimiento prevent_del_upt()
+CREATE OR REPLACE TRIGGER denegation_del_upt_mascota
+BEFORE DELETE OR UPDATE 
+ON mascota
+FOR EACH ROW
+EXECUTE PROCEDURE prevent_del_upt();
+--Pruebas para verificar el resultado del trigger denegation_del_upt_mascota
+DELETE FROM mascota 
+WHERE nomMascota = 'Peirce';
+
+DELETE FROM mascota;
+SELECT * FROM mascota;
+
+UPDATE mascota
+SET especie = 'serpiente',
+raza = 'Golden retriever',
+edad = 12
+WHERE nomMascota = 'Peirce'
