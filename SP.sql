d3a91c618b9efec05b5146d2130ea85bd6d04928
Jesús Alberto Reyes Gutiérrez <alberto.r@ciencias.unam.mx>
2022-05-25 23:28:50 -0500

    Agrega condición de no símbolos

--- SP.sql
+++ SP.sql
@@ -12,7 +12,9 @@ CREATE OR REPLACE PROCEDURE registrarVeterinario(curp VARCHAR,idEstetica INT,
 									fechaInicio DATE, fechaTermino DATE)
 	AS $$
 	BEGIN
-		IF (TRUE) THEN
+		IF (apellidoM ~* '[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]' &&
+			apellidoP ~* '[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]' &&
+			nombre ~* '[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]') THEN
 			INSERT INTO veterinario(curp,idEstetica,apellidoM,apellidoP,nombre,estado,calle,numero,cp,telefono,genero,nacimiento,salario,rfc,inicio,fin,noPacientes,fechaInicio,fechaTermino)
 		 	VALUES (curp,idEstetica,apellidoM,apellidoP,nombre,estado,calle,numero,cp,telefono,genero,nacimiento,salario,rfc,inicio,fin,noPacientes,fechaInicio,fechaTermino);
 		END IF;
@@ -21,7 +23,7 @@ CREATE OR REPLACE PROCEDURE registrarVeterinario(curp VARCHAR,idEstetica INT,
 	Language plpgsql;
 
 CALL registrarVeterinario('ZHXY752436AAIJJQ83',26,'Barnhart','Chadd','Faye','Zacatecas','Rockefeller',23,16060,
-						  3045636873,'F','1985-07-09'::date,12264,'MEPR796726K32','9:58 AM','12:16 AM',2,'2009-08-22'::date,'2017-03-28'::date);
+						  '3045636873','F','1985-07-09'::date,12264,'MEPR796726K32','9:58 AM','12:16 AM',2,'2009-08-22'::date,'2017-03-28'::date);
 
 SELECT * FROM veterinario WHERE curp = 'ZHXY752436AAIJJQ83';
 
